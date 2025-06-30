1bb506ab9b1c223ee688c5c0a2c3afbcf8e2896f
Nicholas Scott Campos <nicholasscott.campos@GFITFTLAPT04266.ad.mynt.xyz>
2025-06-30 14:45:55 +0800

    Added Celcius to Fahrenheit

---
+++ README.md
@@ -0,0 +1,7 @@
+Name: Nicholas Scott Campos
+
+Lab 1: Celsius to Fahrenheit Converter
+
+![img.png](img.png)
+
+![img_1.png](img_1.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,20 @@ package org.example;
 import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void celsiusToFahrenheit(float num){
+        double fahrenheit = 0.0;
 
+        fahrenheit = num * ( 9.0 / 5.0 ) + 32.0;
+        System.out.printf("Temperature in Fahrenheit: %.2f", fahrenheit);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float celsius = 0.0f;
+
+        System.out.print("Enter temperature in Celsius: ");
+        celsius = scanner.nextFloat();
+        celsiusToFahrenheit(celsius);
     }
 }
